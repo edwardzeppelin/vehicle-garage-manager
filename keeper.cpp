cb4282733b3a3279dc21bcdffca5d4c3390b2341
Edward <bigfloppagigachad@gmail.com>
2024-09-05 17:05:41 +0300

    Байты на классы

--- keeper.cpp
+++ keeper.cpp
@@ -1,8 +1,69 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
 class Keeper
 {
 
+};
+
+class Garage
+{
+private:
+	string brand;
+	string model;
+public:
+
+	Garage(string brand, string model)
+	{
+		this->brand = brand;
+		this->model = model;
+	}
+
+	string getbrand() const { return brand; }
+	string getmodel() const { return model; }
+};
+
+class Car: public Garage
+{
+private:
+	float enginev;
+	string colour;
+	string gearbox;
+public:
+	Car(string brand, string model, float enginev, string colour, string gearbox): Garage(brand, model)
+	{
+		this->enginev = enginev;
+		this->colour = colour;
+		this->gearbox = gearbox;
+	}
+};
+
+class Motorcycle : public Garage
+{
+private:
+	float enginev;
+	int horsepwr;
+	string typeofterrain;
+public:
+	Motorcycle(string brand, string model, float enginev, int horsepwr, string typeofterrain) : Garage(brand, model)
+	{
+		this->enginev = enginev;
+		this->horsepwr = horsepwr;
+		this->typeofterrain = typeofterrain;
+	}
+};
+
+class Bus : public Garage
+{
+private:
+	int sitseats;
+	int allseats;
+	string destination;
+public:
+	Bus(string brand, string model, int sitseats, int allseats, string destination) : Garage(brand, model)
+	{
+		this->sitseats = sitseats;
+		this->allseats = allseats;
+		this->destination = destination;
+	}
 };
